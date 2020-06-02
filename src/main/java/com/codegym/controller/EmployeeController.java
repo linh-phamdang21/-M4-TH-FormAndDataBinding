87b0189cc33f7e97ad4b5e8353d5e00ad8d9014f
linh-phamdang21 <ph.danglinh214@gmail.com>
2020-06-02 14:49:02 +0700

    Init project

---
+++ src/main/java/com/codegym/controller/EmployeeController.java
@@ -0,0 +1,35 @@
+package com.codegym.controller;
+
+import com.codegym.model.Employee;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class EmployeeController {
+
+    @GetMapping()
+    public String home(){
+        return "employee/home";
+    }
+
+    @GetMapping("/showform")
+    public String showForm(ModelMap model) {
+        model.addAttribute("employee", new Employee());
+        return "employee/create";
+    }
+
+    @RequestMapping(value = "/addEmployee", method = RequestMethod.POST)
+    public String submit(@ModelAttribute("employee") Employee employee, BindingResult result, ModelMap model) {
+        model.addAttribute("name", employee.getName());
+        model.addAttribute("contactNumber", employee.getContactNumber());
+        model.addAttribute("id", employee.getId());
+        return "employee/infor";
+    }
+}
